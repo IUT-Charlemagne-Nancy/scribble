cc7ac0cd9824f4eea25ffd2b45ed48869de9104c
Benjamin Denys <benjamin.denys7@etu.univ-lorraine.fr>
2015-05-11 17:24:26 +0200

    "Ajout de la classe point"

---
+++ src/Point.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ * @author Benjamin
+ *
+ */
+public class Point {
+	private int x;
+	private int y;
+	
+	public Point(int x1, int y1){
+		this.x = x1;
+		this.y = y1;
+	}
+	
+	public double distance(Point p){
+		double distance = Math.abs(Math.sqrt(Math.pow(p.rendreX()-this.x, 2)+(Math.pow(p.rendreY()-this.y, 2))));
+		return distance;
+	}
+	
+	public int rendreX(){
+		return this.x;
+	}
+	
+	public int rendreY(){
+		return this.y;
+	}
+	
+	public void incrementerX(int nb){
+		if (this.x+nb<0){
+			this.x = 0;
+		}else{
+			this.x=this.x+nb;
+		}		
+	}
+	
+	public void incrementerY(int nb){
+		if (this.y+nb<0){
+			this.y = 0;
+		}else{
+			this.y=this.x+nb;
+		}		
+	}
+	
+	public void modifierX(int nb){
+		this.x = nb;
+	}
+	
+	public void ModifierY(int nb){
+		this.y = nb;
+	}
+	
+	public void translation(int x1, int y1){
+		this.modifierX(x1);
+		this.ModifierY(y1);
+	}
+}
